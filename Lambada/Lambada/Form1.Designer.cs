017c2114da5943af4bbee8286abc9639cf3edf0b
Grzegorz Wojtiuk <grzesio.woj98@gmail.com>
2020-03-27 10:00:30 +0100

    Testowanie przed wykładem

---
+++ Lambada/Lambada/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Lambada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xTB = new System.Windows.Forms.TextBox();
+            this.ObliczbBTN = new System.Windows.Forms.Button();
+            this.yTB = new System.Windows.Forms.TextBox();
+            this.wynikTB = new System.Windows.Forms.RichTextBox();
+            this.Zad1RB = new System.Windows.Forms.RadioButton();
+            this.Zad2RB = new System.Windows.Forms.RadioButton();
+            this.Zad3RB = new System.Windows.Forms.RadioButton();
+            this.Zad4RB = new System.Windows.Forms.RadioButton();
+            this.Zad5RB = new System.Windows.Forms.RadioButton();
+            this.Zad6RB = new System.Windows.Forms.RadioButton();
+            this.Zad7RB = new System.Windows.Forms.RadioButton();
+            this.Zad8RB = new System.Windows.Forms.RadioButton();
+            this.Zad9RB = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // xTB
+            // 
+            this.xTB.Location = new System.Drawing.Point(46, 111);
+            this.xTB.Name = "xTB";
+            this.xTB.Size = new System.Drawing.Size(100, 20);
+            this.xTB.TabIndex = 0;
+            // 
+            // ObliczbBTN
+            // 
+            this.ObliczbBTN.Location = new System.Drawing.Point(174, 202);
+            this.ObliczbBTN.Name = "ObliczbBTN";
+            this.ObliczbBTN.Size = new System.Drawing.Size(75, 23);
+            this.ObliczbBTN.TabIndex = 1;
+            this.ObliczbBTN.Text = "Oblicz";
+            this.ObliczbBTN.UseVisualStyleBackColor = true;
+            this.ObliczbBTN.Click += new System.EventHandler(this.ObliczbBTN_Click);
+            // 
+            // yTB
+            // 
+            this.yTB.Location = new System.Drawing.Point(46, 137);
+            this.yTB.Name = "yTB";
+            this.yTB.Size = new System.Drawing.Size(100, 20);
+            this.yTB.TabIndex = 3;
+            // 
+            // wynikTB
+            // 
+            this.wynikTB.Location = new System.Drawing.Point(296, 57);
+            this.wynikTB.Name = "wynikTB";
+            this.wynikTB.Size = new System.Drawing.Size(201, 168);
+            this.wynikTB.TabIndex = 4;
+            this.wynikTB.Text = "";
+            // 
+            // Zad1RB
+            // 
+            this.Zad1RB.AutoSize = true;
+            this.Zad1RB.Location = new System.Drawing.Point(556, 57);
+            this.Zad1RB.Name = "Zad1RB";
+            this.Zad1RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad1RB.TabIndex = 5;
+            this.Zad1RB.TabStop = true;
+            this.Zad1RB.Text = "Zad1";
+            this.Zad1RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad2RB
+            // 
+            this.Zad2RB.AutoSize = true;
+            this.Zad2RB.Location = new System.Drawing.Point(556, 81);
+            this.Zad2RB.Name = "Zad2RB";
+            this.Zad2RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad2RB.TabIndex = 6;
+            this.Zad2RB.TabStop = true;
+            this.Zad2RB.Text = "Zad2";
+            this.Zad2RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad3RB
+            // 
+            this.Zad3RB.AutoSize = true;
+            this.Zad3RB.Location = new System.Drawing.Point(556, 105);
+            this.Zad3RB.Name = "Zad3RB";
+            this.Zad3RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad3RB.TabIndex = 7;
+            this.Zad3RB.TabStop = true;
+            this.Zad3RB.Text = "Zad3";
+            this.Zad3RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad4RB
+            // 
+            this.Zad4RB.AutoSize = true;
+            this.Zad4RB.Location = new System.Drawing.Point(556, 129);
+            this.Zad4RB.Name = "Zad4RB";
+            this.Zad4RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad4RB.TabIndex = 8;
+            this.Zad4RB.TabStop = true;
+            this.Zad4RB.Text = "Zad4";
+            this.Zad4RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad5RB
+            // 
+            this.Zad5RB.AutoSize = true;
+            this.Zad5RB.Location = new System.Drawing.Point(556, 153);
+            this.Zad5RB.Name = "Zad5RB";
+            this.Zad5RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad5RB.TabIndex = 9;
+            this.Zad5RB.TabStop = true;
+            this.Zad5RB.Text = "Zad5";
+            this.Zad5RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad6RB
+            // 
+            this.Zad6RB.AutoSize = true;
+            this.Zad6RB.Location = new System.Drawing.Point(556, 177);
+            this.Zad6RB.Name = "Zad6RB";
+            this.Zad6RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad6RB.TabIndex = 10;
+            this.Zad6RB.TabStop = true;
+            this.Zad6RB.Text = "Zad6";
+            this.Zad6RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad7RB
+            // 
+            this.Zad7RB.AutoSize = true;
+            this.Zad7RB.Location = new System.Drawing.Point(556, 201);
+            this.Zad7RB.Name = "Zad7RB";
+            this.Zad7RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad7RB.TabIndex = 11;
+            this.Zad7RB.TabStop = true;
+            this.Zad7RB.Text = "Zad7";
+            this.Zad7RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad8RB
+            // 
+            this.Zad8RB.AutoSize = true;
+            this.Zad8RB.Location = new System.Drawing.Point(556, 225);
+            this.Zad8RB.Name = "Zad8RB";
+            this.Zad8RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad8RB.TabIndex = 12;
+            this.Zad8RB.TabStop = true;
+            this.Zad8RB.Text = "Zad8";
+            this.Zad8RB.UseVisualStyleBackColor = true;
+            // 
+            // Zad9RB
+            // 
+            this.Zad9RB.AutoSize = true;
+            this.Zad9RB.Location = new System.Drawing.Point(556, 249);
+            this.Zad9RB.Name = "Zad9RB";
+            this.Zad9RB.Size = new System.Drawing.Size(50, 17);
+            this.Zad9RB.TabIndex = 13;
+            this.Zad9RB.TabStop = true;
+            this.Zad9RB.Text = "Zad9";
+            this.Zad9RB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Zad9RB);
+            this.Controls.Add(this.Zad8RB);
+            this.Controls.Add(this.Zad7RB);
+            this.Controls.Add(this.Zad6RB);
+            this.Controls.Add(this.Zad5RB);
+            this.Controls.Add(this.Zad4RB);
+            this.Controls.Add(this.Zad3RB);
+            this.Controls.Add(this.Zad2RB);
+            this.Controls.Add(this.Zad1RB);
+            this.Controls.Add(this.wynikTB);
+            this.Controls.Add(this.yTB);
+            this.Controls.Add(this.ObliczbBTN);
+            this.Controls.Add(this.xTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xTB;
+        private System.Windows.Forms.Button ObliczbBTN;
+        private System.Windows.Forms.TextBox yTB;
+        private System.Windows.Forms.RichTextBox wynikTB;
+        private System.Windows.Forms.RadioButton Zad1RB;
+        private System.Windows.Forms.RadioButton Zad2RB;
+        private System.Windows.Forms.RadioButton Zad3RB;
+        private System.Windows.Forms.RadioButton Zad4RB;
+        private System.Windows.Forms.RadioButton Zad5RB;
+        private System.Windows.Forms.RadioButton Zad6RB;
+        private System.Windows.Forms.RadioButton Zad7RB;
+        private System.Windows.Forms.RadioButton Zad8RB;
+        private System.Windows.Forms.RadioButton Zad9RB;
+    }
+}
+
---
+++ Lambada/Lambada/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lambada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ObliczbBTN_Click(object sender, EventArgs e)
+        {
+            if (Zad1RB.Checked) Zad1();
+            else if (Zad2RB.Checked) Zad2();
+            else if (Zad3RB.Checked) Zad3();
+            else if (Zad4RB.Checked) Zad4();
+            else if (Zad5RB.Checked) Zad5();
+            else if (Zad6RB.Checked) Zad6();
+            else if (Zad7RB.Checked) Zad7();
+            else if (Zad8RB.Checked) Zad8();
+            else if (Zad9RB.Checked) Zad9();
+
+            
+        }
+        public void Zad1()
+        {
+            wynikTB.Clear();
+            double n = double.Parse(xTB.Text);
+            Func<double, double> nazwa = x => n;
+            wynikTB.Text = nazwa(double.Parse(xTB.Text)).ToString();
+        }
+        public void Zad2()
+        {
+            wynikTB.Clear();
+            string napis2 = yTB.Text;
+            Func<string, string> polacz = napis => napis+" "+napis2;
+            wynikTB.Text = polacz(xTB.Text);
+        }
+        public void Zad3()
+        {
+            wynikTB.Clear();
+            char a;
+            int n = int.Parse(xTB.Text);
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                a = (char)rand.Next(97, 123);
+                Func<char, char> nazwa = x => a;
+                char b= nazwa(char.Parse(xTB.Text));
+                wynikTB.Text += b.ToString();               
+            }
+        }
+
+        public void Zad4()
+        {
+            wynikTB.Clear();
+            int n = 10;
+           double[] tab = new double[10];
+           for(int i = 0; i < n; i++)
+            {
+                Func<double, double> nazwa = x => Math.Pow(x, i);
+                tab[i] = nazwa(double.Parse(xTB.Text));
+                wynikTB.Text += (i + 1) + ". " + tab[i] + "\n";
+            }
+        }
+        public void Zad5()
+        {
+
+        }
+        public void Zad6()
+        {
+
+        }
+        public void Zad7()
+        {
+
+        }
+        public void Zad8()
+        {
+
+        }
+        public void Zad9()
+        {
+
+        }
+
+
+    }
+}
